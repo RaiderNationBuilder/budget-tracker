dfd901666c0f73bff897ac0d4f896cc15e110ef1
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2022-01-16 16:16:22 -0600

    Deploying

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://DaddyBentley:prFVEdXE2d9Pbq6@cluster0.j9gvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
